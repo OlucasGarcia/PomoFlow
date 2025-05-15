5c698feaa27e8757d2d30a36e5f99dc28e05a51d
Lucas Gabriel Garcia <lucasgabrielg287@gmail.com>
2025-05-15 15:36:31 -0300

    Criação das rotas da aplicação

--- PomoFlow/src/main.jsx
+++ PomoFlow/src/main.jsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import Home from './pages/home/Home.jsx'
+
 import './index.css'
 import App from './App.jsx'
 
+const router = createBrowserRouter ([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: 'error',
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+    ]
+  }
+])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
