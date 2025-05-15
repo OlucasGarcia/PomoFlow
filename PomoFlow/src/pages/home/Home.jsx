77de8718744e0b7d7b180888566aa20e9cb7d742
Lucas Gabriel Garcia <lucasgabrielg287@gmail.com>
2025-05-15 16:25:57 -0300

    Estrutura inicial da page Home criada

---
+++ PomoFlow/src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import styles from './Home.module.css'
+
+function Home () {
+    return (
+        <body className={styles.mainDiv}>
+            POMOFLOW
+        </body>
+    )
+}
+
+export default Home;
