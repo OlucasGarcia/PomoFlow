082ec7a28eb662ba1e117da6ee950ab20f4e1f02
Lucas Gabriel Garcia <lucasgabrielg287@gmail.com>
2025-05-17 18:13:12 -0300

    T001: feat - Criação e estilização geral do componente ButtonIniciar

---
+++ PomoFlow/src/components/buttons/buttonIniciar/index.jsx
@@ -0,0 +1,11 @@
+import styles from './style.module.css'
+
+function ButtonIniciar() {
+    return (
+        <button className={styles.btnIniciar}>
+            <p>INICIAR</p>
+        </button>
+    )
+}
+
+export default ButtonIniciar;
