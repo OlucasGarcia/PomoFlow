a5bd34a85c02dba7c326e3f74a2039b9c6a254f4
Lucas Gabriel Garcia <lucasgabrielg287@gmail.com>
2025-05-31 14:44:37 -0300

    T003: chore - preenchimento de estrutura e estilização da page Home

--- PomoFlow/src/pages/home/Home.jsx
+++ PomoFlow/src/pages/home/Home.jsx
@@ -1,34 +1,59 @@
-
 import styles from './Home.module.css'
 
-import PomoFlowLogo from '../../assets/PomoFlowLogo.svg'
-import ButtonIniciar from '../../components/buttons/buttonIniciar';
+import Welcome from '../../components/layout/welcome';
+import Pomoflow from '../../components/layout/pomoflow';
+import TasksList from '../../components/layout/tasksList';
+import SaibaMais from '../../components/layout/saibaMais';
+import Footer from '../../components/layout/footer';
+
+import { useRef, useState } from 'react';
 
 
 function Home() {
 
-    const texto = "Fluxo e foco, tudo no seu ritmo!"
-    const letras = texto.split("");
+    const pomoflowRef = useRef(null)
+    const [showPomoflow, setShowPomoflow] = useState(false)
+    const [animatePomoflow, setAnimatePomoflow] = useState(false);
+    const [contentGrow, setContentGrow] = useState(false);
+
+    const fnScroll = () => {
+        if (!showPomoflow) {
+            setShowPomoflow(true);
+            setContentGrow(true);
+
+            setTimeout(() => {
+                setAnimatePomoflow(true);
+                setTimeout(() => {
+
+                    pomoflowRef.current?.scrollIntoView({ behavior: 'smooth' });
+                })
+
+            })
+        } else {
+            pomoflowRef.current?.scrollIntoView({ behavior: 'smooth' });
+        }
+
+    }
 
     return (
-        <section className={styles.mainDiv}>
-            <div className={styles.lux}></div>
+        <main className={styles.mainDiv}>
 
-            <div className={styles.content}>
+            <div className={`${styles.content} ${contentGrow ? styles.contentFull : ''}`}>
 
-                <h2>
-                    {
-                        letras.map((letra, index) => (
-                            <span key={index} className={styles.letra}>{letra}</span>
-                        ))
-                    }
-                </h2>
-                <img src={PomoFlowLogo} alt="PomoFlow" />
-                <ButtonIniciar />
+                <div className={styles.welcome}>
+                    <Welcome onClick={fnScroll} />
+                </div>
+
+                {showPomoflow && (
+                    <section className={`${styles.pomoflow} ${animatePomoflow ? styles.animate : ''}`} ref={pomoflowRef}>
+                        <Pomoflow />
+                        <TasksList />
+                        <SaibaMais />
+                        <Footer />
+                    </section>
+                )}
             </div>
-
-            <div className={`${styles.lux} ${styles.luxBottom}`}></div>
-        </section>
+        </main>
     )
 }
 
