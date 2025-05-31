5f8bab2db3e1f020c6beaa3f652d9e74d91d2758
Lucas Gabriel Garcia <lucasgabrielg287@gmail.com>
2025-05-31 14:43:56 -0300

    T003: chore - Criação de dataset para Cards dinâmicos

---
+++ PomoFlow/src/dataset/cardContents.jsx
@@ -0,0 +1,26 @@
+import Foco from '../assets/img/foco.png'
+import Organization from '../assets/img/organization.png'
+import Ritmo from '../assets/img/ritmo.png'
+
+const CARDCONTENTS = [
+    {
+        id: "1",
+        img: Foco,
+        title: "Foco absoluto em cada sessão",
+        desc: "Concentre-se totalmente em suas tarefas! O Pomoflow utiliza o método Pomodoro para dividir seu tempo em ciclos de alta produtividade e pequenas pausas, mantendo seu foco renovado a cada sessão e ajudando a evitar distrações que atrapalham seu rendimento."
+    },
+    {
+        id: "2",
+        img: Organization,
+        title: "Organize seu tempo",
+        desc: "Transforme a maneira como você organiza o seu dia! Com o Pomoflow, você planeja e executa suas atividades dentro de blocos de tempo estruturados, aumentando sua eficiência, clareza de prioridades e sensação de progresso constante, sem sobrecarregar sua mente."
+    },
+    {
+        id: "3",
+        img: Ritmo,
+        title: "Supere a procrastinação",
+        desc: "Deixe a procrastinação de lado! O Pomoflow guia você através de sessões cronometradas e pausas estratégicas, com alertas que sinalizam o momento ideal para manter o ritmo ou descansar. Assim, você constrói hábitos mais saudáveis e realiza suas metas com muito mais facilidade."
+    }
+]
+
+export default CARDCONTENTS;
