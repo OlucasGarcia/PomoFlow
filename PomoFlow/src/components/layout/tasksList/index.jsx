040c168b2aefb908ea933652eb36e18eac86e679
Lucas Gabriel Garcia <lucasgabrielg287@gmail.com>
2025-06-23 14:38:11 -0300

    T005: feat - alteração na lógica para funcionamento do componente de layout TaskList

--- PomoFlow/src/components/layout/tasksList/index.jsx
+++ PomoFlow/src/components/layout/tasksList/index.jsx
@@ -1,20 +1,59 @@
-import styles from './style.module.css'
+import styles from './style.module.css';
 
-import ButtonAddTask from '../../buttons/buttonAddTask'
-import Task from '../../form/task'
-import Modal from '../../form/modal'
+import ButtonAddTask from '../../buttons/buttonAddTask';
+import Task from '../../form/task';
+import ModalCreater from '../../form/modal/modalCreater';
+import ModalEditor from '../../form/modal/modalEditor';
 
-function TasksList(props) {
+import useTarefas from '../../../hooks/useTarefas';
+import { useState } from 'react';
+
+function TasksList() {
+
+    const {
+        tarefas,
+        adicionarTarefa,
+        editarTarefa,
+        removerTarefa,
+        alternarConclusao
+    } = useTarefas();
+
+    const [tarefaSelecionada, setTarefaSelecionada] = useState(null);
+
     return (
         <div className={styles.tasksDiv}>
             <p>TAREFAS</p>
-            <ButtonAddTask 
-            onClick={() => {Modal.showModal()}}
+
+            <ButtonAddTask
             />
-            <Task />
+
+            {
+                tarefas.map(tarefa => (
+                    <Task
+                        key={tarefa.id}
+                        desc={tarefa.desc}
+                        concluida={tarefa.concluida}
+                        onChange={() => { alternarConclusao(tarefa.id) }}
+                        onClick={() => {
+                            setTarefaSelecionada(tarefa);
+                            document.getElementById('modalEdit').show();
+                        }}
+                    />
+                ))
+            }
 
             <dialog id='modal'>
-                <Modal />
+                <ModalCreater
+                    adicionarTarefa={adicionarTarefa}
+                />
+            </dialog>
+
+            <dialog id='modalEdit'>
+                <ModalEditor
+                    tarefa={tarefaSelecionada}
+                    editarTarefa={editarTarefa}
+                    removerTarefa={removerTarefa}
+                />
             </dialog>
         </div>
     )
