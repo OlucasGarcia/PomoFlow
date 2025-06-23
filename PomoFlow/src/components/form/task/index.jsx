2995cfd937eab9096ae3a7545b02b4b63ef1243a
Lucas Gabriel Garcia <lucasgabrielg287@gmail.com>
2025-06-23 14:35:55 -0300

    T005: feat - transformação do componente task em funcional e mudança no estilo

--- PomoFlow/src/components/form/task/index.jsx
+++ PomoFlow/src/components/form/task/index.jsx
@@ -4,11 +4,12 @@ import PencilIcon from '../../../assets/icons/pencilIcon.svg';
 function Task(props) {
     return (
         <div className={styles.task}>
-            <input type="checkbox" onChange={props.onChange}/>
-            <p>
-                Exemplo de Task
+            <input type="checkbox" onChange={props.onChange} checked={props.concluida}/>
+            <p className={props.concluida ? styles.done : ''}>
+                {props.desc}
             </p>
-            <button onClick={props.onClick}>
+            <button 
+            onClick={props.onClick}>
                 <img src={PencilIcon} alt="Editar Task" />
             </button>
         </div>
